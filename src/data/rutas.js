2a921e9ae7591fcdc074878797be4624c76171e4
Enrique <eredondoco.inf@upsa.es>
2023-10-25 19:52:12 +0200

    develop: artículos a partir de json

---
+++ src/data/rutas.js
@@ -0,0 +1,114 @@
+
+export const rutas = [
+    {
+        id: 0,
+        titulo: "Ruta 2",
+        subtitulo: "Subtítulo de la ruta",
+        introduccion: "Introducción de la ruta",
+        etiqueta: "Etiqueta de la ruta",
+        pasos: [
+            {
+                titulo: "Paso 1",
+                descripcion: "Esta es la descripción del paso 1",
+                links: [
+                    {
+                        nombre: "Enlace A",
+                        url: "https://www.google.es"
+                    },
+                    {
+                        nombre: "Enlace B",
+                        url: "https://www.google.es"
+                    }
+                ]
+            },
+            {
+                titulo: "Paso 2",
+                descripcion: "Esta es la descripción del paso 2",
+                links: [
+                    {
+                        nombre: "Enlace A",
+                        url: "https://www.google.es"
+                    },
+                    {
+                        nombre: "Enlace B",
+                        url: "https://www.google.es"
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        id: 1,
+        titulo: "Ruta 2",
+        subtitulo: "Subtítulo de la ruta",
+        introduccion: "Introducción de la ruta",
+        etiqueta: "Etiqueta de la ruta",
+        pasos: [
+            {
+                titulo: "Paso 1",
+                descripcion: "Esta es la descripción del paso 1",
+                links: [
+                    {
+                        nombre: "Enlace A",
+                        url: "https://www.google.es"
+                    },
+                    {
+                        nombre: "Enlace B",
+                        url: "https://www.google.es"
+                    }
+                ]
+            },
+            {
+                titulo: "Paso 2",
+                descripcion: "Esta es la descripción del paso 2",
+                links: [
+                    {
+                        nombre: "Enlace A",
+                        url: "https://www.google.es"
+                    },
+                    {
+                        nombre: "Enlace B",
+                        url: "https://www.google.es"
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        id: 2,
+        titulo: "Ruta 3",
+        subtitulo: "Subtítulo de la ruta",
+        introduccion: "Introducción de la ruta",
+        etiqueta: "Etiqueta de la ruta",
+        pasos: [
+            {
+                titulo: "Paso 1",
+                descripcion: "Esta es la descripción del paso 1",
+                links: [
+                    {
+                        nombre: "Enlace A",
+                        url: "https://www.google.es"
+                    },
+                    {
+                        nombre: "Enlace B",
+                        url: "https://www.google.es"
+                    }
+                ]
+            },
+            {
+                titulo: "Paso 2",
+                descripcion: "Esta es la descripción del paso 2",
+                links: [
+                    {
+                        nombre: "Enlace A",
+                        url: "https://www.google.es"
+                    },
+                    {
+                        nombre: "Enlace B",
+                        url: "https://www.google.es"
+                    }
+                ]
+            },
+        ]
+    }
+]
